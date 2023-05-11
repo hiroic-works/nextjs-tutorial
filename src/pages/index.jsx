9c247b92da0cb359b213d5d3d41db2ce4902bd6d
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-11 17:16:07 +0900

    ✨ useStateで配列を追加するサンプル

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -32,6 +33,25 @@ export default function Home() {
     setText(e.target.value);
   };
 
+  const handleAdd = useCallback(() => {
+    if (!text) {
+      alert("何か入力してください");
+      return;
+    }
+
+    // 本来ミュータブルな配列やオブジェクトをReactではイミュータブルに扱う必要があるため、
+    // push()などを用いて元の配列を破壊的に変更する事はできず、スプレッド構文を使用して非破壊的に新しい配列として返さなければならない
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ値は入れられません");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+
+    setText("");
+  }, [text]);
+
   useEffect(() => {
     // ※本来はDOMに直接アクセスしてはいけない。どうしても必要な場合はuseRef()を使う。
     document.body.style.backgroundColor = "lightblue";
@@ -51,7 +71,16 @@ export default function Home() {
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
-
+      <br />
+      <br />
+      <div>
+        <button onClick={handleAdd}>追加</button>
+      </div>
+      <ul>
+        {array.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
 
       <Footer />
