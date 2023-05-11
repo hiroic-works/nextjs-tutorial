dea4994006a77f73515c3f77702a07175ec76a6f
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-11 14:54:51 +0900

    ⚗️ クリックイベントサンプル。※不要な記述でいつか消す

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,15 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    console.log(e.target.href);
+    alert(foo);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +20,9 @@ export default function Home() {
       </Head>
 
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
 
       <Main page="index" />
 
