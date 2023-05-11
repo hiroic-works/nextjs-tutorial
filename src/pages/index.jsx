e538c14f1246013b68c4a7152af2a3dfa95e60ce
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-11 15:53:18 +0900

    ⚡️ useCallback、useEffectの第二引数についてのサンプル

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,18 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = () => {
-    // ※直接countにカウントを加えず、関数の引数で前のcountの値が取得できるのでそれに対してカウントを加える
-    setCount((prevCount) => prevCount + 1);
-  };
+
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      // ※直接countにカウントを加えず、関数の引数で前のcountの値が取得できるのでそれに対してカウントを加える
+      setCount((prevCount) => prevCount + 1);
+    }
+    // 第二引数に指定した変数を監視して再生成する。空配列を指定したら最初にレンダリングされる時だけとなるためcountは1のままになる
+  }, [count]);
 
   useEffect(() => {
     // ※本来はDOMに直接アクセスしてはいけない。どうしても必要な場合はuseRef()を使う。
