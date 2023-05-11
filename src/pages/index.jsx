b7a27e58d09320e6a8e1d83e915e4550a51a50b8
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-11 15:26:48 +0900

    ✨ カウンターを設置してuseStateを使用してカウントアップを実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,22 +3,19 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    e.preventDefault();
-    console.log(e.target.href);
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = () => {
+    // ※直接countにカウントを加えず、関数の引数で前のcountの値が取得できるのでそれに対してカウントを加える
+    setCount((prevCount) => prevCount + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント");
     // ※本来はDOMに直接アクセスしてはいけない。どうしても必要な場合はuseRef()を使う。
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("アンマウント");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -30,9 +27,8 @@ export default function Home() {
       </Head>
 
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
 
