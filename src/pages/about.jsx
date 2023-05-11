8f54ee70616a3a620e9b8d96a24dbea6f73b78bf
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-11 14:37:12 +0900

    🏗️ 拡張子、ディレクトリ構成、インポート方法の変更、_appを用いた処理共通化

---
+++ src/pages/about.jsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import styles from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>about page</title>
+        <meta name="description" content="about page" />
+      </Head>
+
+      <Header />
+
+      <Main page="about" />
+
+      <Footer />
+    </div>
+  );
+}
