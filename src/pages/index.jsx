b055561cd3628eee011942e3a3657ef34f9c0aa1
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-11 16:19:57 +0900

    ✨ useStateで文字列と真偽値について解説。useCallbackの補足

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,14 +7,30 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       // ※直接countにカウントを加えず、関数の引数で前のcountの値が取得できるのでそれに対してカウントを加える
       setCount((prevCount) => prevCount + 1);
     }
-    // 第二引数に指定した変数を監視して再生成する。空配列を指定したら最初にレンダリングされる時だけとなるためcountは1のままになる
+    // 第二引数に指定した変数を監視して再生成する。
+    // 空配列を指定したら最初にレンダリングされる時だけ再生成となるためcountは1のまま更新されない。
+    // ※ただ、setCount()の引数のprevCountは前回のcountを参照して更新しているため、こちらの値は更新され続ける
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
+
+  const handleChange = (e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内までです");
+      return;
+    }
+    setText(e.target.value);
+  };
 
   useEffect(() => {
     // ※本来はDOMに直接アクセスしてはいけない。どうしても必要な場合はuseRef()を使う。
@@ -31,8 +47,10 @@ export default function Home() {
       </Head>
 
       <Header />
-      <h1>{count}</h1>
+      {isShow && <h1>{count}</h1>}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
 
       <Main page="index" />
 
