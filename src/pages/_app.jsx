7b5f3898370175906fbb6096f9a3b97486605202
h.oda <62193961+hiroic-works@users.noreply.github.com>
2023-05-15 15:14:56 +0900

    ✨ ページ間のステートのリフトアップについてのサンプル

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,21 @@
 import Head from "next/head";
 import "src/styles/globals.css";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
+// ステートのリフトアップ（親で定義して各子コンポーネントに渡してステートの共通化を図る）
 function MyApp({ Component, pageProps }) {
+  const counter = useCounter();
+  const inputArray = useInputArray();
+
+  useBgLightBlue();
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} {...counter} {...inputArray} />
     </>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,20 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
-export default function Home() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
+export default function Home(props) {
+  // propsからによる分割代入
+  const {
+    count,
+    isShow,
+    handleClick,
+    handleDisplay,
+    text,
+    array,
+    handleChange,
+    handleAdd,
+  } = props;
 
-  useBgLightBlue();
   return (
     <div className={styles.container}>
       <Head>
